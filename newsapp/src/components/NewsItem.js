17a65e2a7b3100ef3d9ae9322717aad6b0a13c74
Harshal Thaware <125807305+HarshGit1824@users.noreply.github.com>
2024-10-29 06:21:08 +0000

    Create card to store news-items

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
+    let { title, description } = this.props;
     return (
       <div>
-        This is a News Item
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="https://ichef.bbci.co.uk/news/1024/branded_sport/ff96/live/2d940320-93a0-11ef-876b-0b878ef874c5.jpg"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/newsdetail" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
