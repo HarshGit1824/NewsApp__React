ceb15aaf748d1a9c76591b3682059042308002ca
Harshal Thaware <125807305+HarshGit1824@users.noreply.github.com>
2024-11-06 11:29:07 +0000

    Fetch news from newsapi

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export class NewsItem extends Component {
 
   render() {
-    let { title, description, imageUrl } = this.props;
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
@@ -11,9 +11,9 @@ export class NewsItem extends Component {
             src={imageUrl}
           />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="/newsdetail" className="btn btn-sm  btn-primary">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description} ...</p>
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
               Read More
             </a>
           </div>
