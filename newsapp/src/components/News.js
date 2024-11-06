f175c4dcf38a794cae4cee2acb92d0c745181bef
Harshal Thaware <125807305+HarshGit1824@users.noreply.github.com>
2024-11-06 05:43:43 +0000

    imageUrl set

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export class News extends Component {
   render() {
@@ -7,21 +7,19 @@ export class News extends Component {
       <div className="container my-3">
         <h2>NewsBite - Top Headlines</h2>
         <div className="row">
-        <div className="col-md-4">
-             <NewsItem title="myTitle" description="mydesc"/>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="mydesc" imageUrl="https://ichef.bbci.co.uk/news/1024/branded_sport/ff96/live/2d940320-93a0-11ef-876b-0b878ef874c5.jpg"/>
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="mydesc" />
+          </div>
         </div>
-        <div className="col-md-4">
-             <NewsItem title="myTitle" description="mydesc"/>
-        </div>
-        <div className="col-md-4">
-             <NewsItem title="myTitle" description="mydesc"/>
-        </div>
-        </div>
-
-
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -1,21 +1,20 @@
 import React, { Component } from "react";
 
 export class NewsItem extends Component {
+
   render() {
-    let { title, description } = this.props;
+    let { title, description, imageUrl } = this.props;
     return (
-      <div>
+      <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
           <img
-            src="https://ichef.bbci.co.uk/news/1024/branded_sport/ff96/live/2d940320-93a0-11ef-876b-0b878ef874c5.jpg"
-            className="card-img-top"
-            alt="..."
+            src={imageUrl}
           />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsdetail" className="btn btn-primary">
-              Go somewhere
+            <a href="/newsdetail" className="btn btn-sm  btn-primary">
+              Read More
             </a>
           </div>
         </div>
