e93c9e9069285b406502bfea77caf8a73253d661
Harshal Thaware <125807305+HarshGit1824@users.noreply.github.com>
2024-11-06 14:14:16 +0000

    Add prev nd next buttons functionality

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -8,12 +8,13 @@ export class NewsItem extends Component {
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
           <img
-            src={imageUrl}
+            src={!imageUrl?"https://techcrunch.com/wp-content/uploads/2024/05/ipad-noplay.png?resize=1200,675":imageUrl}
+            alt=""
           />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description} ...</p>
-            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">
               Read More
             </a>
           </div>
