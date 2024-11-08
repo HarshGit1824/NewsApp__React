74f6906c88e977549ea2f385ee407ceeaae6e635
Harshal Thaware <125807305+HarshGit1824@users.noreply.github.com>
2024-11-08 04:25:18 +0000

    Update style changes

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
     let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <div className="my-3">
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card">
           <img
             src={!imageUrl?"https://media.istockphoto.com/id/1313303632/video/breaking-news-template-intro-for-tv-broadcast-news-show-program-with-3d-breaking-news-text.jpg?s=640x640&k=20&c=S0dTZp37XKVcCAnoguMnRatvv4Nkp2cjmA5aYOOrJs8=":imageUrl}
             alt=""
