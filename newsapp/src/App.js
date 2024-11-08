311849c96946f6af08990ea3332f89d76a914c2a
Harshal Thaware <125807305+HarshGit1824@users.noreply.github.com>
2024-11-08 04:42:08 +0000

    Setting up Router

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 export default class App extends Component {
 
   render() {
