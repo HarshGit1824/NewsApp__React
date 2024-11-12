c0f81ccf29ba37727a5ec765557ca044f7d5a249
Harshal Thaware <125807305+HarshGit1824@users.noreply.github.com>
2024-11-12 03:28:47 +0000

    Adding Infinite Scroll

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -1,47 +1,48 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
-import PropTypes from 'prop-types';
-
+import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
-
   static defaultProps = {
     pageSize: 5,
-    country: 'us',
-    category: 'general'
-  }
+    country: "us",
+    category: "general",
+  };
 
   static propTypes = {
     pageSize: PropTypes.number,
     country: PropTypes.string,
     category: PropTypes.string,
-  }
+  };
 
-  capitalizeFirstLetter = (string)=> {
+  capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
-  }
+  };
   constructor(props) {
     super(props);
     this.state = {
       articles: [],
       loading: true,
       page: 1,
-    }
-    document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsBite`;
+      totalResults: 0,
+    };
+    document.title = `${this.capitalizeFirstLetter(
+      this.props.category
+    )} - NewsBite`;
   }
 
-  async updateNews(){
-    const url =
-      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=10b79f44a3d7408db2e9b5f3e2a8a99a&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({loading: true});
+  async updateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=10b79f44a3d7408db2e9b5f3e2a8a99a&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
-      loading: false
+      loading: false,
     });
   }
 
@@ -51,59 +52,67 @@ export class News extends Component {
 
   handlePrevClick = async () => {
     // console.log("Previous");
-    this.setState({page: this.state.page - 1})
+    this.setState({ page: this.state.page - 1 });
     this.updateNews();
   };
   handleNextClick = async () => {
     // console.log("Next");
-    this.setState({page: this.state.page + 1})
+    this.setState({ page: this.state.page + 1 });
     this.updateNews();
+  };
 
+  fetchMoreData = async () => {
+    this.setState({ page: this.state.page + 1 });
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=10b79f44a3d7408db2e9b5f3e2a8a99a&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
   };
 
   render() {
     return (
-      <div className="container my-3">
-        <h1 className="text-center" style={{margin: '24px 0px'}}>NewsBite &mdash; Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
-        {this.state.loading && <Spinner/>}
-        <div className="row">
-          {!this.state.loading && this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <NewsItem
-                  title={element.title ? element.title : ""}
-                  description={element.description ? element.description : ""}
-                  imageUrl={element.urlToImage}
-                  newsUrl={element.url}
-                  author={element.author}
-                  date={element.publishedAt}
-                  source={element.source.name}
-                />
-              </div>
-            );
-          })}
-        </div>
+      <>
+        <h1 className="text-center" style={{ margin: "24px 0px" }}>
+          NewsBite &mdash; Top {this.capitalizeFirstLetter(this.props.category)}{" "}
+          Headlines
+        </h1>
+        {/* {this.state.loading && <Spinner/>} */}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
+          <div className="container">
+            <div className="row">
+              {this.state.articles.map((element) => {
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <NewsItem
+                      title={element.title ? element.title : ""}
+                      description={
+                        element.description ? element.description : ""
+                      }
+                      imageUrl={element.urlToImage}
+                      newsUrl={element.url}
+                      author={element.author}
+                      date={element.publishedAt}
+                      source={element.source.name}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </InfiniteScroll>
 
-        <div className="container d-flex justify-content-between">
-          <button
-            disabled={this.state.page <= 1}
-            type="button"
-            className="btn btn-dark mx-2"
-            onClick={this.handlePrevClick}
-          >
-            ⏮️ Previous
-          </button>
-
-          <button
-          disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)}
-            type="button"
-            className="btn btn-dark mx-2"
-            onClick={this.handleNextClick}
-          >
-            Next ⏭️
-          </button>
-        </div>
-      </div>
+      </>
     );
   }
 }
